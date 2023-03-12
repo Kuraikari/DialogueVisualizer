2710604aa910e8e5a0c7c7a69f59498d40aa9c58
Kuraikari <zwermi@gmail.com>
2023-03-12 11:57:12 +0100

    version: 0.1.0.0
    minor changes

---
+++ Dialogue Visualizer/DialoguesServiceLibrary/Services/DialogueDbContext.cs
@@ -0,0 +1,35 @@
+﻿using DialoguesServiceLibrary.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DialoguesServiceLibrary.Services
+{
+    internal class DialogueDbContext : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+        public DialogueDbContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (optionsBuilder.IsConfigured)
+            {
+                return;
+            }
+            else
+            {
+                optionsBuilder.UseSqlite(Configuration.GetConnectionString("WebApiDatabase"));
+            }
+        }
+
+        public DbSet<Dialogue> _dialogues { get; set; }
+    }
+}
--- Dialogue Visualizer/DialoguesServiceLibrary/Services/DialogueService.cs
+++ Dialogue Visualizer/DialoguesServiceLibrary/Services/DialogueService.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace DialoguesServiceLibrary.Services
 {
-    internal class DialogueService
+    public class DialogueService
     {
     }
 }
